d933108d1ff1757145170245d85297aff49ee158
Ryuya Uetake <38969179+deruloxxx@users.noreply.github.com>
2023-08-17 22:29:15 +0800

    feat: 11.2.4

--- src/main/java/com/example/practicespringboot/app/config/SecurityConfig.java
+++ src/main/java/com/example/practicespringboot/app/config/SecurityConfig.java
@@ -3,33 +3,36 @@ package com.example.practicespringboot.app.config;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 // Chore SecurityConfigについてちゃんと学習する
 public class SecurityConfig {
   @Bean
-  public org.springframework.security.web.SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.formLogin(login -> login
         .loginProcessingUrl("/login") // フォーム認証の設定記述開始
         .loginPage("/login") // ユーザー名・パスワードの送信先URL
-        .defaultSuccessUrl("/") // ログイン成功後のリダイレクト先URL
-        .failureUrl("/error") // ログイン失敗後のリダイレクト先URL
+        .failureUrl("/login?error") // ログイン失敗後のリダイレクト先URL
+        .defaultSuccessUrl("/user/list", true) // ログイン成功後のリダイレクト先URL
         .permitAll() // ログイン画面は未ログインでもアクセス可能
       )
       .logout(logout -> logout
         .logoutSuccessUrl("/")
       )
-      .authorizeHttpRequests(authz -> authz
+      .authorizeHttpRequests(auth -> auth
         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
         .requestMatchers(PathRequest.toH2Console()).permitAll()
-        .requestMatchers("/login").permitAll()
+        .requestMatchers("/general").hasRole("GENERAL")
+        .requestMatchers("/admin").hasRole("ADMIN")
         .requestMatchers("/user/signup").permitAll()
         .anyRequest().authenticated()
       );
-    // CSRF対策を無効に設定(一時的)
     return http.build();
   }
 }
